5f905732ff1462a01ca578a268d87a8bebce0eeb
Thorben Bender <bender.thorben0@gmail.com>
2019-02-11 14:49:58 +0100

    working on social cards

--- Project-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
@@ -1,2 +1,14 @@
 import React from 'react';
 import './Card.css';
+import CardContent from './CardContent';
+import CardBanner from './CardBanner';
+const Cardcontainer = () => {
+    return(
+        <div>
+            <CardBanner />
+            <CardContent />
+        </div>
+    )
+}
+
+export default Cardcontainer;
--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -1,2 +1,19 @@
 import React from 'react';
 import './Header.css';
+import HeaderThumbnail from './ImageThumbnail';
+import HeaderContent from './HeaderContent';
+import HeaderTitle from './HeaderTitle';
+
+const HeaderContainer = () => {
+    return(
+        <div className="HeaderContainer">
+            <HeaderThumbnail />
+            <div className="HeaderContent">
+                <HeaderTitle />
+                <HeaderContent />
+            </div>
+        </div>
+    )
+}
+
+export default HeaderContainer;
