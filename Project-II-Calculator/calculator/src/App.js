88e329cb0d1dc4b3e327d106a226b7f55c5c07af
Thorben Bender <bender.thorben0@gmail.com>
2019-02-12 15:29:02 +0100

    styled the calculator

--- Project-II-Calculator/calculator/src/App.js
+++ Project-II-Calculator/calculator/src/App.js
@@ -3,18 +3,40 @@ import './App.css';
 import CalculatorDisplay from '../src/components/DisplayComponents/CalculatorDisplay';
 import NumberField from '../src/components/ButtonComponents/NumberField';
 import ActionButton from '../src/components/ButtonComponents/ActionButton';
+import RedButton from '../src/components/ButtonComponents/red-button';
 
 const App = () => {
   return (
     <div className="Calculator">
       <CalculatorDisplay />
-      <ActionButton
-        desiredClass= 'actionbutton'
-        button= "Clear" />
-      <NumberField />
-      <ActionButton
-        desiredClass="actionbutton"
-        button= "0" />
+      <div className="button-field">
+        <div className="left-side">
+          <ActionButton
+            desiredClass= 'actionbutton'
+            button= "Clear" />
+          <NumberField />
+          <ActionButton
+            desiredClass="actionbutton"
+            button= "0" />
+        </div>
+        <div className="right-side">
+          <RedButton 
+            className= 'Red-Button'
+            type='/' />
+            <RedButton 
+            className= 'Red-Button'
+            type='x' />
+            <RedButton 
+            className= 'Red-Button'
+            type='-' />
+            <RedButton 
+            className= 'Red-Button'
+            type='+' />
+            <RedButton 
+            className= 'Red-Button'
+            type='=' />
+        </div>
+      </div>
     </div>
   );
 };
---
+++ Project-II-Calculator/calculator/src/components/ButtonComponents/red-button.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+
+export default function RedButton({type , className}){
+    return (
+        <div className="red-button">
+            {type}
+        </div>
+    );
+};
